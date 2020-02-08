e066fba9fe15a5988e16d4097fc48f466077d314
Ivo Slováček <ivoslovacek@gmail.com>
2020-02-08 23:57:45 +0100

    generating random numbers

--- Program.cs
+++ Program.cs
@@ -10,30 +10,76 @@ namespace sudokualgorythm
     {
         static void Main(string[] args)
         {
-            int[,] playGrid = gridMaker()
-            int[, , ] checkGrid = new int[9, 9, 10];
-            int[, ] defaultGrid = playGrid;
+            int[,] unsolvedGrid = new int[9, 9];
+            int[,] solvedGrid = gridMaker();
             Console.WriteLine();
+            for (int i = 0; i < 9; i++)
+            {
+                for (int n = 0; n < 9; n++)
+                {
+                    Console.WriteLine(solvedGrid[n, i]);
+                }
+            }
             Console.ReadLine();
 
 
         }
 
-        static int[,] gridSolution(int[,] playGrid, int[,,] checkGrid, int[,] default Grid)
+        static int randomNumber()
         {
-
+            Random random = new Random();
+            return random.Next(1, 9);
         }
 
-        static int[,] gridMaker(int[,] playGrid)
+        static int[,] gridMaker()
+        {
+            int generatedNumber = randomNumber();
+            int[,] playGrid = new int[9, 9]; 
+            for (int i = 0; i < 9; i++)
+            {
+                for (int n = 0; n < 9; n++)
+                {
+                    playGrid[i, n] = 0;
+                }
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                for (int n = 0; n < 9; n++)
+                {
+                    if (gridChecker(generatedNumber, playGrid, n, i)==0)
+                    {
+                        playGrid[i, n] = generatedNumber;
+                        generatedNumber = randomNumber();
+                    }
+                    else
+                    {
+                        n--;
+                        generatedNumber = randomNumber();
+                    }
+                }
+            }
+            return playGrid;
+        }
+
+        static int gridChecker(int generatedNumber, int[,] playGrid, int a, int b)
         {
             for (int i = 0; i < 9; i++)
             {
                 for (int n = 0; n < 9; n++)
                 {
-                    playGrid[n, i] = 0;
+                    if(generatedNumber == playGrid[b, n])
+                    {
+                        Console.Write(generatedNumber);
+                        return 1;
+                    }
+                }
+                if (generatedNumber == playGrid[i, a])
+                {
+                    Console.Write(generatedNumber);
+                    return 1;
                 }
             }
-            return playGrid;
+            return 0;
         }
     }
 }
